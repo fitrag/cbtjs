069dacf43ed655448dd59d7a5fe3ada15456a2ef
fitrag <jaya88114@gmail.com>
2025-01-09 16:00:13 +0700

    update

--- src/app/pages/home/page.js
+++ src/app/pages/home/page.js
@@ -4,13 +4,45 @@ const Home = () => {
     return (
         <>
         <Header/>
-            <div className="flex flex-col m-4">
-                <div className="flex shadow bg-white rounded-lg p-5 my-2">
+            <div className="flex flex-col m-4 mb-[90px]">
+                <div className="flex shadow space-x-4 items-center bg-white rounded-lg p-5 my-2">
                     <div className="w-[100px]">
-                        <div className="bg-blue-300 p-3 rounded-lg h-[100px] flex items-center justify-center">ABV</div>
+                        <div className="bg-blue-200 p-3 rounded-lg h-[100px] flex items-center justify-center font-bold text-lg text-white text-3xl">RPL</div>
                     </div>
-                    <div className="flex-1 px-3">
-                        <h3 className="font-semibold text-gray-800">PTS Rekayasa Perangkat Lunak</h3>
+                    <div className="flex-1">
+                        <h3 className="font-bold text-lg text-gray-800">PTS Rekayasa Perangkat Lunak</h3>
+                        <h3 className="text-gray-500 text-sm">Fadila Fitra Kusuma Jaya</h3>
+                        <div className="flex-1 bg-blue-500 text-center text-white p-2 rounded-lg mt-4 text-sm">Mulai ujian</div>
+                    </div>
+                </div>
+                <div className="flex shadow space-x-4 items-center bg-white rounded-lg p-5 my-2">
+                    <div className="w-[100px]">
+                        <div className="bg-blue-200 p-3 rounded-lg h-[100px] flex items-center justify-center font-bold text-lg text-white text-3xl">DKV</div>
+                    </div>
+                    <div className="flex-1">
+                        <h3 className="font-bold text-lg text-gray-800">PTS Dasar Desain Grafis</h3>
+                        <h3 className="text-gray-500 text-sm">Muhammad Shohibul Anam</h3>
+                        <div className="flex-1 bg-blue-500 text-center text-white p-2 rounded-lg mt-4 text-sm">Mulai ujian</div>
+                    </div>
+                </div>
+                <div className="flex shadow space-x-4 items-center bg-white rounded-lg p-5 my-2">
+                    <div className="w-[100px]">
+                        <div className="bg-blue-200 p-3 rounded-lg h-[100px] flex items-center justify-center font-bold text-lg text-white text-3xl">DKV</div>
+                    </div>
+                    <div className="flex-1">
+                        <h3 className="font-bold text-lg text-gray-800">PTS Dasar Desain Grafis</h3>
+                        <h3 className="text-gray-500 text-sm">Muhammad Shohibul Anam</h3>
+                        <div className="flex-1 bg-blue-500 text-center text-white p-2 rounded-lg mt-4 text-sm">Mulai ujian</div>
+                    </div>
+                </div>
+                <div className="flex shadow space-x-4 items-center bg-white rounded-lg p-5 my-2">
+                    <div className="w-[100px]">
+                        <div className="bg-blue-200 p-3 rounded-lg h-[100px] flex items-center justify-center font-bold text-lg text-white text-3xl">DKV</div>
+                    </div>
+                    <div className="flex-1">
+                        <h3 className="font-bold text-lg text-gray-800">PTS Dasar Desain Grafis</h3>
+                        <h3 className="text-gray-500 text-sm">Muhammad Shohibul Anam</h3>
+                        <div className="flex-1 bg-blue-500 text-center text-white p-2 rounded-lg mt-4 text-sm">Mulai ujian</div>
                     </div>
                 </div>
             </div>
